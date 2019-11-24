8ccd43609689868ad02dcbe623d2e1cb8d31d27c
andreyzavrichko <andrey@zavrichko.dev>
2019-11-24 13:04:09 +0200

    add task1 (tag <p>)

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <?php
+require "src/function.php";
+echo task1(array('Петров','Иван', 'Семенович'), true);
--- src/function.php
+++ src/function.php
@@ -1,1 +1,11 @@
 <?php
+function task1($array, $parameter)
+{
+    if ($parameter) {
+        return implode(' ', $array);
+    } else {
+        foreach ($array as $value) {
+            echo "<p>$value</p>";
+        }
+    }
+}
